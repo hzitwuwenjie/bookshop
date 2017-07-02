71aca5114e6a31a119da27895f23ab56f4c3f148
clark1230 <3267672721@qq.com>
2017-07-02 11:48:49 +0800

    tianjiayige user

---
+++ bookshop-web/src/main/java/com/hzit/bookshop/web/User.java
@@ -0,0 +1,10 @@
+package com.hzit.bookshop.web;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello hzit");
+    }
+}
